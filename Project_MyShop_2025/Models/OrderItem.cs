87551a407e1306239e0b353767b0b214f86b1929
23122039 <122670158+trungkiet2005@users.noreply.github.com>
2026-01-13 15:36:59 +0700

    feat: orderitem entity data

---
+++ Project_MyShop_2025/Models/OrderItem.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Project_MyShop_2025.Models
+{
+    public class OrderItem
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int OrderId { get; set; }
+        [ForeignKey(nameof(OrderId))]
+        public Order? Order { get; set; }
+
+        public int ProductId { get; set; }
+        [ForeignKey(nameof(ProductId))]
+        public Product? Product { get; set; }
+
+        public int Quantity { get; set; }
+
+        public double Price { get; set; } // Price at the time of purchase
+    }
+}
