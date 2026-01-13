2805d964295166280c77ef932f4d111fdd7959ea
23122039 <122670158+trungkiet2005@users.noreply.github.com>
2026-01-13 15:37:23 +0700

    feat: prodict  entity data

---
+++ Project_MyShop_2025/Models/Product.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Project_MyShop_2025.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        public string? Description { get; set; }
+
+        [Range(0, double.MaxValue)]
+        public double Price { get; set; }
+
+        public string? Image { get; set; }
+
+        public int Quantity { get; set; }
+
+        public int CategoryId { get; set; }
+
+        [ForeignKey(nameof(CategoryId))]
+        public Category? Category { get; set; }
+    }
+}
