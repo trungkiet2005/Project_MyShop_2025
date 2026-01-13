f00d0c668f85a695a5fed2af87ab601d19dd0e2a
23122039 <122670158+trungkiet2005@users.noreply.github.com>
2026-01-13 19:05:00 +0700

    add: OrderStatus enum in to model

---
+++ Project_MyShop_2025.Core/Migrations/20260113115706_AddOrderStatus.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Project_MyShop_2025.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOrderStatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Orders",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Orders");
+        }
+    }
+}
