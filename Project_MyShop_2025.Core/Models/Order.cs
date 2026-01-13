6ce1627d43024a4157b85151e93fa1212a752091
23122039 <122670158+trungkiet2005@users.noreply.github.com>
2026-01-13 19:05:53 +0700

    migrate: status field into orders

--- Project_MyShop_2025.Core/Models/Order.cs
+++ Project_MyShop_2025.Core/Models/Order.cs
@@ -4,6 +4,13 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Project_MyShop_2025.Core.Models
 {
+    public enum OrderStatus
+    {
+        Created = 0,    // Mới tạo
+        Paid = 1,       // Đã thanh toán
+        Cancelled = 2   // Đã hủy
+    }
+
     public class Order
     {
         [Key]
@@ -12,6 +19,8 @@ namespace Project_MyShop_2025.Core.Models
         public DateTime CreatedAt { get; set; } = DateTime.Now;
 
         public int TotalPrice { get; set; }
+
+        public OrderStatus Status { get; set; } = OrderStatus.Created;
 
         public string? CustomerName { get; set; }
         public string? CustomerPhone { get; set; }
