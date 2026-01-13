56269fc703b3d4611477988e1a549b593df31f82
23122039 <122670158+trungkiet2005@users.noreply.github.com>
2026-01-13 15:36:39 +0700

    feat: order entity data

---
+++ Project_MyShop_2025/Models/Order.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Project_MyShop_2025.Models
+{
+    public class Order
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public double TotalPrice { get; set; }
+
+        public string? CustomerName { get; set; }
+        public string? CustomerPhone { get; set; }
+        public string? CustomerAddress { get; set; }
+
+        public ICollection<OrderItem> Items { get; set; } = new List<OrderItem>();
+    }
+}
