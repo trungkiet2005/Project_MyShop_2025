d246412525d013b2c176d3381aab0439a4fef059
23122041 <duyminh12122005@gmail.com>
2026-01-19 08:38:58 +0700

    refactor(auth): Update PasswordHelper to HMACSHA512 hashing

--- Project_MyShop_2025.Core/Helpers/PasswordHelper.cs
+++ Project_MyShop_2025.Core/Helpers/PasswordHelper.cs
@@ -6,24 +6,26 @@ namespace Project_MyShop_2025.Core.Helpers
 {
     public static class PasswordHelper
     {
-        public static string HashPassword(string password)
+        public static void CreatePasswordHash(string password, out string passwordHash, out string passwordSalt)
         {
-            using (var sha256 = SHA256.Create())
+            using (var hmac = new HMACSHA512())
             {
-                var bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
-                var builder = new StringBuilder();
-                foreach (var b in bytes)
-                {
-                    builder.Append(b.ToString("x2"));
-                }
-                return builder.ToString();
+                passwordSalt = Convert.ToBase64String(hmac.Key);
+                passwordHash = Convert.ToBase64String(hmac.ComputeHash(Encoding.UTF8.GetBytes(password)));
             }
         }
 
-        public static bool VerifyPassword(string password, string hash)
+        public static bool VerifyPasswordHash(string password, string storedHash, string storedSalt)
         {
-            var hashOfInput = HashPassword(password);
-            return StringComparer.OrdinalIgnoreCase.Compare(hashOfInput, hash) == 0;
+            if (string.IsNullOrWhiteSpace(password) || string.IsNullOrWhiteSpace(storedHash) || string.IsNullOrWhiteSpace(storedSalt))
+                return false;
+
+            using (var hmac = new HMACSHA512(Convert.FromBase64String(storedSalt)))
+            {
+                var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+                var computedHashString = Convert.ToBase64String(computedHash);
+                return computedHashString == storedHash;
+            }
         }
     }
 }
